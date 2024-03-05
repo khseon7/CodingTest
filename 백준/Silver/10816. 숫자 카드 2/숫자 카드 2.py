3cecbe8b35755fbbf520b9c8f4eadd8a5f3ea95e
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-03-05 17:04:10 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 708 ms, Memory: 133092 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,18 +1,19 @@
 import sys
 n_dict=dict()
 n=int(sys.stdin.readline())
-n_list=list(map(int,sys.stdin.readline().split()))
-for i in n_list:
-  if i in n_dict:
-    n_dict[i]+=1
-  else:
-    n_dict[i]=1
+for i in list(map(int,sys.stdin.readline().split())):
+    if i in n_dict.keys():
+        n_dict[i]+=1
+    else:
+        n_dict[i]=1
 m=int(sys.stdin.readline())
 m_list=list(map(int,sys.stdin.readline().split()))
+# 상근이가 어떤 카드를 몇개 가지고 있는지 나타내는 card_list 생성
 card_list=[]
+# m_list에서 각 항들이 n_dict의 key값 중 하나라면 해당 key의 value값을 str 문자형으로 바꿔서 card_list에 추가
 for i in m_list:
-  if i in n_dict:
-    card_list.append(str(n_dict.get(i)))
-  else:
-    card_list.append("0")
+    if i in n_dict.keys():
+        card_list.append(str(n_dict.get(i)))
+    else:
+        card_list.append("0")
 print(' '.join(card_list))

ba86b73ff174ab05b39721b67216d10a5a0e3aa1
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-03-07 23:00:16 +0900

    [Silver III] Title: 파일 정리, Time: 200 ms, Memory: 41104 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -2,9 +2,10 @@ import sys
 n=int(input())
 extent_dict=dict()
 for _ in range(n):
-  file_name=input().split('.')
-  if file_name[1] in extent_dict.keys():
-    extent_dict[file_name[1]]+=1
+  file_name=sys.stdin.readline().split('.')
+  extent=file_name[1].rstrip()
+  if extent in extent_dict.keys():
+    extent_dict[extent]+=1
   else:
-    extent_dict[file_name[1]]=1
+    extent_dict[extent]=1
 for i in sorted(extent_dict.keys()):  print(i,extent_dict[i])

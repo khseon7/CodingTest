e3a95dd7126795d1bcc5acba70794f6657e91f08
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-03-26 19:15:33 +0900

    [Silver V] Title: 기술 연계마스터 임스, Time: 88 ms, Memory: 32680 KB -BaekjoonHub

---
+++ 백준/Silver/25497. 기술 연계마스터 임스/기술 연계마스터 임스.py
@@ -0,0 +1,25 @@
+import sys
+n=int(sys.stdin.readline())
+input_list=list(sys.stdin.readline().rstrip())
+pre_skill={'L':0,'S':0,'R':0,'K':0}
+cnt=0
+for i in input_list:
+  if i=='L':
+    pre_skill['L']+=1
+  elif i=='S':
+    pre_skill['S']+=1
+  elif i=='R':
+    if pre_skill['L']>0:
+      pre_skill['L']-=1
+      pre_skill['R']+=1
+    else:
+      break
+  elif i=='K':
+    if pre_skill['S']>0:
+      pre_skill['S']-=1
+      pre_skill['K']+=1
+    else:
+      break
+  elif i>='1' and i<='9':
+    cnt+=1
+print(cnt+pre_skill['R']+pre_skill['K'])

b03c3279e1af966c8104edea5f1b70628c7ca4a1
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-07-18 14:03:16 +0900

    [Gold V] Title: A와 B, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -3,11 +3,9 @@ t=list(input())
 while(len(s)<len(t)):
   if(t[-1]=='A'):
     t.pop()
-    # print(f"'A' : {t}")
   elif(t[-1]=='B'):
     t.pop()
     t.reverse()
-    # print(f"'B' : {t}")
 if(s==t):
   print(1)
 else:

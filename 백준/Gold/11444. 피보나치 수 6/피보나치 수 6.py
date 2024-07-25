1e70cc2be6fd459e32aa92b0a6b3ac0a3013a97e
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-07-25 18:39:29 +0900

    [Gold II] Title: 피보나치 수 6, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -0,0 +1,25 @@
+def power(arr_1,arr_2):
+  res=[0,0,0,0]
+  for i in range(2):
+    for j in range(2):
+      for k in range(2):
+        res[i*2+j]+=arr_1[i*2+k]*arr_2[k*2+j]
+      res[i*2+j]%=1000000007
+  return res
+
+front=[1,1,1,0]
+res=[1,0,0,1]
+
+n = int(input())
+if n == 0:
+    print(0)
+elif n == 1:
+    print(1)
+else:
+    n -= 1
+    while n != 0:
+        if n % 2 == 1:
+            res = power(res, front)
+        front = power(front, front)
+        n //= 2
+    print(res[0])

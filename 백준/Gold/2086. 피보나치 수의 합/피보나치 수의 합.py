142ec7dd9f935e1f94c3157aaf7c8a280af56fbc
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-07-29 11:01:40 +0900

    [Gold I] Title: 피보나치 수의 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2086. 피보나치 수의 합/피보나치 수의 합.py
@@ -0,0 +1,28 @@
+def power(arr_1,arr_2):
+  res=[0,0,0,0]
+  for i in range(2):
+    for j in range(2):
+      for k in range(2):
+        res[i*2+j]+=arr_1[i*2+k]*arr_2[k*2+j]
+      res[i*2+j]%=1000000000
+  return res
+
+def fibo(n):
+    front=[1,1,1,0]
+    res=[1,0,0,1]
+
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        n -= 1
+        while n != 0:
+            if n % 2 == 1:
+                res = power(res, front)
+            front = power(front, front)
+            n//=2
+        return res[0]
+
+a,b=map(int,input().split())
+print((fibo(b+2)-fibo(a+1))%1000000000)

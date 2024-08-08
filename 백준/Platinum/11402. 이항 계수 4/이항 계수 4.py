6db8ceb13d903ab0f55d08cc73d6da3c3417b352
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-08-08 17:25:56 +0900

    [Platinum IV] Title: 이항 계수 4, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Platinum/11402. 이항 계수 4/이항 계수 4.py
+++ 백준/Platinum/11402. 이항 계수 4/이항 계수 4.py
@@ -7,16 +7,20 @@ def lucas(n,k):
   for i in range(1,k+1):
     x=(x*(n-i+1))//i
   return x
+
+def bino(n,k,m):
+  arr_n=[]
+  arr_k=[]
+  while n or k:
+    arr_n.append(n%m)
+    arr_k.append(k%m)
+    n//=m
+    k//=m
+  res=1
+  for i in range(len(arr_n)):
+    res=res*lucas(arr_n[i],arr_k[i])%m
+  return res
+
 import sys
 n,k,m=map(int,sys.stdin.readline().split())
-arr_n=[]
-arr_k=[]
-while n or k:
-  arr_n.append(n%m)
-  arr_k.append(k%m)
-  n//=m
-  k//=m
-res=1
-for i in range(len(arr_n)):
-  res=res*lucas(arr_n[i],arr_k[i])%m
-print(res)
+print(bino(n,k,m))

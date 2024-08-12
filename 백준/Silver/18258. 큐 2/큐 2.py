67c297881d5581e8eecbde3130692371df28e0e4
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-08-12 18:44:31 +0900

    [Silver IV] Title: 큐 2, Time: 1584 ms, Memory: 110232 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,32 @@
+import sys
+n=int(input())
+arr=[0]*2000000
+front=rear=-1
+for _ in range(n):
+  param=sys.stdin.readline().split()
+  if param[0]=="push":
+    rear+=1
+    arr[rear]=int(param[1])
+  elif param[0]=="pop":
+    if front==rear:
+      print(-1)
+    else:
+      front+=1
+      print(arr[front])
+  elif param[0]=='empty':
+    if front==rear:
+      print(1)
+    else:
+      print(0)
+  elif param[0]=='size':
+    print(rear-front)
+  elif param[0]=='front':
+    if front==rear:
+      print(-1)
+    else:
+      print(arr[front+1])
+  elif param[0]=='back':
+    if front==rear:
+      print(-1)
+    else:
+      print(arr[rear])

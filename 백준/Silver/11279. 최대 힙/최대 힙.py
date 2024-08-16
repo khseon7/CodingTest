a6bc8eb6054ec0ea327df8d253e1cf85de175434
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-08-16 17:18:57 +0900

    [Silver II] Title: 최대 힙, Time: 380 ms, Memory: 38044 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,44 +1,34 @@
 import sys
-heap = [0]  # dummy node to make heap 1-indexed
-res = []
-n = int(sys.stdin.readline())
-
+heap=[]
+heap.append(0)
+res=[]
+n=int(sys.stdin.readline())
 for _ in range(n):
-    x = int(sys.stdin.readline())
-    if x == 0:
-        if len(heap) == 1:
-            res.append("0")
-        else:
-            # Extract max
-            res.append(str(heap[1]))
-            heap[1] = heap[-1]
-            heap.pop()
-            
-            # Heapify down
-            i = 1
-            while True:
-                left = 2 * i
-                right = 2 * i + 1
-                largest = i
-                
-                if left < len(heap) and heap[left] > heap[largest]:
-                    largest = left
-                if right < len(heap) and heap[right] > heap[largest]:
-                    largest = right
-                    
-                if largest != i:
-                    heap[i], heap[largest] = heap[largest], heap[i]
-                    i = largest
-                else:
-                    break
+  x=int(sys.stdin.readline())
+  if x==0:
+    if len(heap)==1:
+      res.append(str(0))
     else:
-        # Insert x
-        heap.append(x)
-        
-        # Heapify up
-        i = len(heap) - 1
-        while i > 1 and heap[i] > heap[i // 2]:
-            heap[i], heap[i // 2] = heap[i // 2], heap[i]
-            i = i // 2
-
+      heap[1],heap[-1]=heap[-1],heap[1]
+      res.append(str(heap.pop()))
+      i=1
+      while True:
+        left=2*i
+        right=2*i+1
+        value=i
+        if left<len(heap) and heap[left]>heap[value]:
+          value=left
+        if right<len(heap) and heap[right]>heap[value]:
+          value=right
+        if value!=i:
+          heap[i],heap[value]=heap[value],heap[i]
+          i=value
+        else:
+          break
+  elif x>0:
+    heap.append(x)
+    i=len(heap)-1
+    while i!=1 and heap[i//2]<heap[i]:
+      heap[i//2],heap[i]=heap[i],heap[i//2]
+      i=i//2
 print("\n".join(res))

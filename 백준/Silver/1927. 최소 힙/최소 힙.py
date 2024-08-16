0a65524049fce0dd3fe04e0d5e9a2b5efd9a2cc5
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-08-16 17:22:55 +0900

    [Silver II] Title: 최소 힙, Time: 408 ms, Memory: 38044 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,34 @@
+import sys
+heap=[]
+heap.append(0)
+res=[]
+n=int(sys.stdin.readline())
+for _ in range(n):
+  x=int(sys.stdin.readline())
+  if x==0:
+    if len(heap)==1:
+      res.append(str(0))
+    else:
+      heap[1],heap[-1]=heap[-1],heap[1]
+      res.append(str(heap.pop()))
+      i=1
+      while True:
+        left=2*i
+        right=2*i+1
+        value=i
+        if left<len(heap) and heap[left]<heap[value]:
+          value=left
+        if right<len(heap) and heap[right]<heap[value]:
+          value=right
+        if value!=i:
+          heap[i],heap[value]=heap[value],heap[i]
+          i=value
+        else:
+          break
+  elif x>0:
+    heap.append(x)
+    i=len(heap)-1
+    while i!=1 and heap[i//2]>heap[i]:
+      heap[i//2],heap[i]=heap[i],heap[i//2]
+      i=i//2
+print("\n".join(res))

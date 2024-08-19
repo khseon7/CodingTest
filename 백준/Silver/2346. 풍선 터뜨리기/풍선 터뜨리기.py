8b2013c2a5ac0a5acdcd3e53fff6f2be85295dae
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-08-19 20:17:04 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 48 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,27 +1,20 @@
 from collections import deque
 import sys
 
-input = sys.stdin.readline
-n = int(input())
-arr = list(map(int, input().split()))
+input=sys.stdin.readline
 
-# 인덱스와 값의 쌍을 저장하는 deque 생성
-dq = deque((arr[i], i + 1) for i in range(n))
-result = []
+n=int(input())
+left_deque=deque()
+arr=list(map(int,input().split()))
+dq=deque((arr[i],i+1)for i in range(n))
+res=[]
 
-while len(dq) > 1:
-    # 현재 위치에서 pop하고 결과에 추가
-    index, pos = dq.popleft()
-    result.append(pos)
-
-    # 남은 요소들이 있을 때만 회전
-    if index > 0:
-        dq.rotate(-(index - 1))  # 시계 방향으로 이동
+while len(dq)!=1:
+    index,posit=dq.popleft()
+    res.append(posit)
+    if index>0:
+        dq.rotate(-index+1)
     else:
-        dq.rotate(-index)  # 반시계 방향으로 이동
-
-# 마지막 남은 요소의 위치 추가
-result.append(dq[0][1])
-
-# 결과 출력
-print(' '.join(map(str, result)))
+        dq.rotate(-index)
+res.append(dq[0][1])
+print(' '.join(map(str,res)))

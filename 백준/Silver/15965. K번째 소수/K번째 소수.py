db265126b69edcdb4872a30e959ae2dc9ab261b7
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-08-22 16:19:32 +0900

    [Silver II] Title: K번째 소수, Time: 2320 ms, Memory: 115608 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.py
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -1,11 +1,15 @@
 import sys
 input=sys.stdin.readline
-arr=[1]*500001
+size=8000001
+arr=[1]*size
 prime_list=[]
-for i in range(2,500001):
+for i in range(2,int(size**0.5)+1):
     if arr[i]:
         prime_list.append(i)
-        for j in range(2*i,500001,i):
+        for j in range(2*i,size,i):
             arr[j]=0
+for i in range((int(size**0.5)+1),size):
+    if arr[i]:
+        prime_list.append(i)
 n=int(input())
 print(prime_list[n-1])

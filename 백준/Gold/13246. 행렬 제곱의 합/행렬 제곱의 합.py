5b152ce370a2a85ddccfed50fc40407b19f5c28f
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-08-23 11:52:42 +0900

    [Gold I] Title: 행렬 제곱의 합, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13246. 행렬 제곱의 합/행렬 제곱의 합.py
+++ 백준/Gold/13246. 행렬 제곱의 합/행렬 제곱의 합.py
@@ -1,47 +1,45 @@
 import sys
 input=sys.stdin.readline
-mod=1000
 n,b=map(int,input().split())
-arr=[list(map(int,input().split())) for _ in range(n)]
+mat=[list(map(int,input().split())) for _ in range(n)]
+mod=1000
 for i in range(n):
     for j in range(n):
-        arr[i][j]%=mod
+        mat[i][j]%=mod
 
 def mul_mat(arr_1,arr_2,mod=1000):
-    n=len(arr)
-    res=[[0]*n for _ in range(n)]
+    n=len(arr_1)
+    res=[[0]*n for i in range(n)]
     for i in range(n):
         for j in range(n):
             for k in range(n):
                 res[i][j]=(res[i][j]+arr_1[i][k]*arr_2[k][j])%mod
     return res
-    
-def power(arr,n):
-    res=[[0]*len(arr) for _ in range(len(arr))]
-    for i in range(len(arr)):
-        res[i][i]=1
-    while n:
-        if n%2:
-            res=mul_mat(res,arr)
-        arr=mul_mat(arr,arr)
-        n//=2
+
+def pow_mat(A,B):
+    res=[[0 if i!=j else 1 for j in range(n)] for i in range(n)]
+    while B:
+        if B%2:
+            res=mul_mat(res,A)
+        A=mul_mat(A,A)
+        B//=2
     return res
 
-def mat_plus(arr,arr_b=None,mod=1000):
-    n=len(arr)
-    if arr_b is None:
-        arr_b=[[0 if i!=j else 1 for j in range(n)] for i in range(n)]
-    return [[(arr[i][j]+arr_b[i][j])%mod for j in range(n)] for i in range(n)]
-    
-def solve(arr,b):
-    if b==1:
-        return arr
-    left_mat=mat_plus(power(arr,b//2))
-    right_mat=solve(arr,b//2)
+def sum_mat(mat_1,mat_2=None,mod=1000):
+    n=len(mat_1)
+    if mat_2 is None:
+        mat_2=[[0 if i!=j else 1 for j in range(n)] for i in range(n)]
+    return [[(mat_1[i][j]+mat_2[i][j])%mod for j in range(n)] for i in range(n)]
+
+def solve(A,B):
+    if B==1:
+        return A
+    left_mat=sum_mat(pow_mat(A,B//2))
+    right_mat=solve(A,B//2)
     res=mul_mat(left_mat,right_mat)
-    if b%2:
-        res=mat_plus(res,power(arr,b))
+    if B%2:
+        res=sum_mat(res,pow_mat(A,B))
     return res
 
-for i in solve(arr,b):
-    print(" ".join(map(str,i)))
+for row in solve(mat,b):
+    print(" ".join(map(str,row)))

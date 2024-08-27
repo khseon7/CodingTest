6a853dfa4524accc2e6279faa2b07dae570ccc2e
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-08-27 11:14:56 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 4684 ms, Memory: 109268 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -0,0 +1,30 @@
+import sys
+input=sys.stdin.readline
+size=10003001
+arr=[1]*(size+1)
+res=[]
+def palindrome(n):
+    cnt=0
+    int_to_srt=str(n)
+    for i in range(len(int_to_srt)//2):
+        if int_to_srt[i]==int_to_srt[-i-1]:
+            cnt+=1
+    if cnt==len(int_to_srt)//2:
+        return True
+    else:
+        return False
+for i in range(2,size+1):
+    if arr[i]:
+        if palindrome(i):
+            res.append(i)
+        for j in range(2*i,size+1,i):
+            arr[j]=0
+n=int(input())
+i=0
+for i in range(len(res)-1):
+    if res[0]>=n:
+        print(res[0])
+        break
+    elif res[i]<n and res[i+1]>=n:
+        print(res[i+1])
+        break

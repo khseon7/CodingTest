112e55e4342efb09270eb7c2c32f3f3884f2ae33
khseon7 <110168316+khseon7@users.noreply.github.com>
2024-09-10 12:57:13 +0900

    [Silver III] Title: 선물, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1166. 선물/선물.py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+n,l,w,h=map(int,input().split())
+
+left,right=0,min(l,w,h)
+for _ in range(100):
+    mid=(left+right)/2
+    if (l//mid)*(w//mid)*(h//mid)>=n:
+        left=mid
+    else:
+        right=mid
+
+print(left)

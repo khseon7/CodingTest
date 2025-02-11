0f0fd2a13590ba3803e3f482802fee93a585cb12
khseon7 <110168316+khseon7@users.noreply.github.com>
2025-02-11 21:20:39 +0900

    [Silver II] Title: 자원 캐기, Time: 80 ms, Memory: 34456 KB -BaekjoonHub

---
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.py
@@ -0,0 +1,20 @@
+import sys
+input=sys.stdin.readline
+
+N,M=map(int,input().split())
+
+arr=[list(map(int,input().split())) for _ in range(N)]
+dp=arr.copy()
+
+for r in range(N):
+    if r==0:
+        for c in range(M-1):
+            dp[r][c+1]=dp[r][c]+arr[r][c+1]
+    else:
+        for c in range(M):
+            if c==0:
+                dp[r][c]=dp[r-1][c]+arr[r][c]
+            else:
+                dp[r][c]=max(dp[r-1][c],dp[r][c-1])+arr[r][c]
+
+print(dp[N-1][M-1])

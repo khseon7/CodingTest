526e38a0b62c0a079d8b4bba0ad373af60418815
김학선 <110168316+khseon7@users.noreply.github.com>
2025-07-20 13:20:34 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,7 +1,8 @@
 n = int(input())
-arr = list(map(int,input().split()))
-arr = sorted(arr)
+arr = sorted(list(map(int,input().split())))
+
 total = 0
 for i in range(n):
     total += arr[i]*(n-i)
+
 print(total)
